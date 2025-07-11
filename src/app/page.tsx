d8e9228f74b94d3de96a5a60669f798780a4bc65
jonathancsr <jonathancsrr@gmail.com>
2025-07-10 21:23:23 -0300

    feat: setup ai jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { toast } from "sonner";
 
 const Page = () => {
+  const [text, setText] = useState("");
   const trpc = useTRPC();
   const invoke = useMutation(
     trpc.invoke.mutationOptions({
@@ -14,11 +17,13 @@ const Page = () => {
       },
     })
   );
+
   return (
     <div className="p-4 max-w-7xl mx-auto">
+      <Input value={text} onChange={(e) => setText(e.target.value)}></Input>
       <Button
         disabled={invoke.isPending}
-        onClick={() => invoke.mutate({ text: "Jonathan" })}
+        onClick={() => invoke.mutate({ value: text })}
         className="pointer"
       >
         Invoke Background Job
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,15 +1,21 @@
+import { createAgent, openai } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    // download step
-    await step.sleep("wait-a-moment", "30s");
-    // processing step
-    await step.sleep("wait-a-moment", "10s");
-    // summaryze step
-    await step.sleep("wait-a-moment", "5s");
-    return { message: `Hello ${event.data.email}!` };
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system:
+        "You are an expert next.js developer.  You write readable, maitainable code. You write simple next.js & react snippets. return the code in a code block, return only the code, no other text.",
+      model: openai({ model: "gpt-4o" }),
+    });
+
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`
+    );
+    //@ts-ignore
+    return { output };
   }
 );
